582f65326fde182ac118007712cf6bfabf75a41a
JulienCharbonneau <juliencharbonneauqc@gmail.com>
2022-10-21 08:54:26 -0400

    fix Door and FloorRequestButton

--- door.go
+++ door.go
@@ -1,9 +1,13 @@
 package main
 
-
 type Door struct {
-	ID int
+	ID     int
 	status string
-
 }
 
+func NewDoor(_ID int, _status string) Door {
+
+	door := Door{ID: +_ID, status: _status}
+
+	return door
+}
--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
-import "fmt"
-
 func main() {
 
-	var newButton = NewFloorRequestButton(1, "up")
-	// // newButton := FloorRequestButton{}
+	// var newDoor = NewDoor(1, "closed")
+	// fmt.Println("door id: ", newDoor.ID)
+	// fmt.Println("door status: ", newDoor.status)
+
+	// var newButton = NewFloorRequestButton(1, "up")
 	// newButton.floor = 1
-	fmt.Println(newButton.floor)
+	// fmt.Println(newButton.floor)
 
 	// var newDoor = Door{ID: 1,status: "closed"}
 	// fmt.Println(newDoor)
